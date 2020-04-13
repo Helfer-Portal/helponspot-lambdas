e836430c106b62764993da05d600e70b8a7317a9
Andreas Voit <AndreasVoit@gmx.de>
2020-04-13 12:32:51 +0200

    Add commons to deployed zip

--- lambdas/post-user/deploy-lambda.sh
+++ lambdas/post-user/deploy-lambda.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 npm run build
-aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_post-user_dev --zip-file fileb://app.zip
+cd ../../
+
+echo "Include commons in app zip"
+zip -r post-user.zip ./lambdas/post-user/dist/** ./lambdas/post-user/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
+
+echo "deploy lambda"
+aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_post-user_dev --zip-file fileb://post-user.zip
+
+rm post-user.zip
--- lambdas/post-user/zip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tsc
-mkdir -p ./foo/bar/common/help-on-spot-models/dist
-cp -r ./../../common/help-on-spot-models/dist/** ./foo/bar/common/help-on-spot-models/dist
-zip -r app.zip ./dist/** ./node_modules/** ./foo/bar/common/help-on-spot-models/dist
-
-rm -r ./common/
-aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_post-user_dev --zip-file fileb://app.zip
