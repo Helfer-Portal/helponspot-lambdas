2417a12fcc9090fbb4283eaabd152279c02282f1
Felix Ohr <felix.ohr@scandio.de>
2020-04-17 15:08:58 +0200

    No sync on db startup; Changed folder path in deploy script

--- lambdas/get-qualifications/deploy-lambda.sh
+++ lambdas/get-qualifications/deploy-lambda.sh
@@ -4,7 +4,7 @@ npm run build
 cd ../../
 
 echo "Include commons in app zip"
-zip -r post-user.zip ./lambdas/post-user/dist/** ./lambdas/post-user/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
+zip -r post-user.zip ./lambdas/get-qualifications/dist/** ./lambdas/get-qualifications/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
 
 echo "deploy lambda"
 aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_get-qualifications_dev --zip-file fileb://post-user.zip
