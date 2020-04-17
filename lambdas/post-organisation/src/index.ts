062df649dd68ac95e503fbe1a1009afe6c3f26c4
Felix Ohr <felix.ohr@scandio.de>
2020-04-17 19:03:08 +0200

    16: Refactored Database class to initalize connection lazy

--- lambdas/post-organisation/src/index.ts
+++ lambdas/post-organisation/src/index.ts
@@ -1,35 +1,23 @@
 require('dotenv').config();
 
+import {LambdaResponse, lambdaResponse} from "../../../common/help-on-spot-models/dist/utils/lambdaResponse";
 import {OrganisationData} from "../../../common/help-on-spot-models/src/models/RestModels";
 import Organisation from "../../../common/help-on-spot-models/dist/entity/Organisation";
 import {User} from "../../../common/help-on-spot-models/dist";
 import {Database} from "../../../common/help-on-spot-models/dist/utils/Database";
 
-
-interface LambdaResponse {
-    isBase64Encoded: boolean
-    statusCode: number
-    body: string
-    headers: any
-}
-
 export interface LambdaInputEvent {
     body: string
 }
-
-const defaultHeader = {
-    'Content-Type': 'application/json'
-}
-
 
 export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> => {
     console.log(JSON.parse(event.body))
     const organisationData: OrganisationData = JSON.parse(event.body)
 
     const db = new Database();
-    const connection = await db.connect();
+    const connection = await db.getConnection();
     if (!connection) {
-        throw Error("Unable to Connect to the database")
+        return lambdaResponse(500, 'No Database connection');
     }
 
     try {
@@ -39,21 +27,11 @@ export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> 
         const organisation = new Organisation(organisationData, users)
         let persistedOrganisation: Organisation = await connection.manager.save(organisation)
         console.log(`Persisted new Organisation: ${JSON.stringify(persistedOrganisation)}`)
-        return {
-            isBase64Encoded: false,
-            statusCode: 200,
-            body: JSON.stringify(persistedOrganisation),
-            headers: defaultHeader
-        }
+        return lambdaResponse(200, JSON.stringify(persistedOrganisation));
     } catch (e) {
         console.log(`Error during lambda execution:\n ${JSON.stringify(e)}`)
-        return {
-            isBase64Encoded: false,
-            statusCode: 500,
-            body: JSON.stringify(e),
-            headers: defaultHeader
-        }
+        return lambdaResponse(500, JSON.stringify(e));
     } finally {
-        await db.disconnect()
+        await db.disconnect(connection);
     }
 }
