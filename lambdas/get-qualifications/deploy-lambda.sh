fe3d7715fe1606d0451132df9150b7a615cf5ddc
Felix Ohr <felix.ohr@scandio.de>
2020-04-17 14:51:41 +0200

    Usage of lambdaResponse util function, added lambda arn to deploy script

--- lambdas/get-qualifications/deploy-lambda.sh
+++ lambdas/get-qualifications/deploy-lambda.sh
@@ -7,6 +7,6 @@ echo "Include commons in app zip"
 zip -r post-user.zip ./lambdas/post-user/dist/** ./lambdas/post-user/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
 
 echo "deploy lambda"
-# aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_post-user_dev --zip-file fileb://post-user.zip
+aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_get-qualifications_dev --zip-file fileb://post-user.zip
 
 rm post-user.zip
--- lambdas/get-qualifications/src/index.ts
+++ lambdas/get-qualifications/src/index.ts
@@ -1,41 +1,20 @@
 require('dotenv').config();
 import {Qualification} from "../../../common/help-on-spot-models/dist";
 import {Database} from "../../../common/help-on-spot-models/dist/utils/Database";
-
-interface LambdaResponse {
-  isBase64Encoded: boolean
-  statusCode: number
-  body: string
-  headers: any
-}
+import {LambdaResponse, lambdaResponse} from "../../../common/help-on-spot-models/dist/utils/lambdaResponse";
 
 export interface LambdaInputEvent {}
-
-const defaultHeader = {
-  'Content-Type': 'application/json'
-}
 
 export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> => {
   const db = new Database();
   const connection = await db.connect();
-  
   try {
       const repo = connection!.getRepository(Qualification)
       const qualifications: Qualification[] = await repo!.find();
-      return {
-        isBase64Encoded: false,
-        statusCode: 200,
-        body: JSON.stringify(qualifications),
-        headers: defaultHeader,
-      };
+      return lambdaResponse(200, JSON.stringify(qualifications));
   } catch (e) {
     console.log(`Error during lambda execution: ${JSON.stringify(e)}`)
-    return {
-      isBase64Encoded: false,
-      statusCode: 500,
-      body: JSON.stringify(e),
-      headers: defaultHeader
-    }
+    return lambdaResponse(500, JSON.stringify(e));
   }
   finally {
       await db.disconnect()
