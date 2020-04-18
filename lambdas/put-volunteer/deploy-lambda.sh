7411317fa5416e4b3acb218ebbc3cf3faa2b127c
Niko Abeler <niko@rerere.org>
2020-04-18 14:01:55 +0200

    activated deploy

--- lambdas/put-volunteer/deploy-lambda.sh
+++ lambdas/put-volunteer/deploy-lambda.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-projectName=post-request
+projectName=put-volunteer
 
 npm i
 npm run build
@@ -10,6 +10,6 @@ echo "Include commons in app zip"
 zip -qq -r ${projectName}.zip ./lambdas/${projectName}/dist/** ./lambdas/${projectName}/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
 
 echo "deploy lambda"
-# aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_${projectName}_dev --zip-file fileb://${projectName}.zip
+aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_${projectName}_dev --zip-file fileb://${projectName}.zip
 
 rm ${projectName}.zip
