955c5c20fce84b25ecc475e4019b385e8f32c00d
Andreas Voit <AndreasVoit@gmx.de>
2020-04-19 21:17:59 +0200

    #9: Filter requests by users qualifications and same city

--- lambdas/get-users-id-requests/src/index.ts
+++ lambdas/get-users-id-requests/src/index.ts
@@ -1,28 +1,63 @@
+import {Qualification, User} from "../../../common/help-on-spot-models/dist";
+
 require('dotenv').config();
-import {User} from "../../../common/help-on-spot-models/dist";
+
+import Request from "../../../common/help-on-spot-models/dist/entity/Request";
 import {Database} from "../../../common/help-on-spot-models/dist/utils/Database";
 import {LambdaResponse, lambdaResponse} from "../../../common/help-on-spot-models/dist/utils/lambdaResponse";
 
 export interface LambdaInputEvent {
-  pathParameters: any
+    pathParameters: {
+        userId: string,
+        radius?: string,
+        requestType?: string,
+        location?: { lat: number, long: number }
+    }
+}
+
+function matchesUserQuals(request: Request, requestedQualifications: Qualification[]): boolean {
+    const userKeys = requestedQualifications.map(r => r.key)
+
+    return request.qualifications!.every(rq => userKeys.includes(rq.key))
 }
 
 export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> => {
-  const userId = event.pathParameters.userId;
-  const db = new Database();
-  const connection = await db.getConnection();
-  try {
-      const repo = connection.getRepository(User);
-      const user: User | undefined = await repo.findOne({ where: { id: userId }, relations: ['address', 'qualifications'] });
-      if (!user) {
-        return lambdaResponse(404, "User not found!");
-      }
-      return lambdaResponse(200, JSON.stringify(user));
-  } catch (e) {
-    console.log(`Error during lambda execution: ${JSON.stringify(e)}`)
-    return lambdaResponse(500, JSON.stringify(e));
-  }
-  finally {
-      await db.disconnect(connection)
-  }
+    const userId = event.pathParameters.userId;
+    const db = new Database();
+    const connection = await db.getConnection();
+    try {
+        console.log('looking for ' + userId)
+        const user = await connection.getRepository(User).findOne({
+            where: {id: userId},
+            relations: ['qualifications', 'address']
+        })
+        if (!user) {
+            return lambdaResponse(400, 'Give User does not exist')
+        }
+        const requestedQualifications: Qualification[]  = user.qualifications!
+        const requestedCity = user.address!.city
+
+        const requestRepository = connection.getRepository(Request)
+        const matchedRequests = await requestRepository.createQueryBuilder("request")
+            .leftJoinAndSelect("request.address", "address")
+            .leftJoinAndSelect("request.qualifications", "qualifications")
+            .where("address.city = :city", {city: requestedCity})
+            .getMany()
+
+        console.log('Found ' + matchedRequests.length + " matches")
+        const fileterd = matchedRequests.filter(request => matchesUserQuals(request, requestedQualifications))
+        console.log(`Remain ${fileterd.length}`)
+        console.log(`Remain ${JSON.stringify(fileterd, null, 2)}`)
+        // const user: User | undefined = await requestRepository.findOne({ where: { id: userId }, relations: ['address', 'qualifications'] });
+
+        if (!matchedRequests) {
+            return lambdaResponse(404, "No Requests match the given query");
+        }
+        return lambdaResponse(200, fileterd);
+    } catch (e) {
+        console.log(`Error during lambda execution: ${e}`)
+        return lambdaResponse(500, JSON.stringify(e));
+    } finally {
+        await db.disconnect(connection)
+    }
 }
--- lambdas/get-users-id-requests/src/test.ts
+++ lambdas/get-users-id-requests/src/test.ts
@@ -1,10 +1,26 @@
 import {handler, LambdaInputEvent} from "./index";
+import {Database} from "../../../common/help-on-spot-models/dist/utils/Database";
+import {Address, Qualification, User} from "../../../common/help-on-spot-models/dist";
+import {AddressData} from "../../../common/help-on-spot-models/dist/models/RestModels";
 
 (async function () {
 
+
+    const connection = await new Database().getConnection();
+    const userRepo = connection.getRepository(User);
+    const qualifications = await connection.getRepository(Qualification).find()
+    const randomEmail = Math.random().toString(36).substring(7) + "@test";
+    const adata: AddressData = {country: "germany", city: "myCity", postalCode: "123", houseNumber: "1", street: "street"}
+    const address = await  connection.getRepository(Address).save(new Address(adata))
+
+    const user = new User("Test", "User", false, randomEmail, "", [qualifications[2]])
+    user.address = address
+    await userRepo.save(user);
+    await connection.close();
+
   const requestObject: LambdaInputEvent = {
     pathParameters: {
-      userId: "91D2E5DD-0CDF-4D1E-83F1-962A95822307"
+      userId: user.id!
     }
   }
 
