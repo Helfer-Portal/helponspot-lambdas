7ecc19fc81424ceed0aac41ebb3ee109d2e3f101
Duc Tung Tong <tungtongduc@gmail.com>
2020-04-23 10:40:52 +0200

    adjust deployment script

--- lambdas/delete-organisation/deploy-lambda.sh
+++ lambdas/delete-organisation/deploy-lambda.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 projectName=delete-organisation
+zipFile=$projectName.zip
 
 npm run build
-cd ../../
 
-echo "Include commons in app zip"
-zip -qq -r ${projectName}.zip ./lambdas/${projectName}/dist/** ./lambdas/${projectName}/node_modules/** ./common/help-on-spot-models/dist ./common/help-on-spot-models/node_modules
+zip -r -j $zipFile ./dist/*
 
 echo "deploy lambda"
-aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_${projectName}_dev --zip-file fileb://${projectName}.zip
+aws lambda update-function-code --function-name arn:aws:lambda:eu-central-1:198891906952:function:HoS_${projectName}_dev --zip-file fileb://$zipFile
 
-rm ${projectName}.zip
+rm $zipFile
