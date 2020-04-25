0a7e6a239e24029912493450fdf27c3db47e7ca0
Milad Khanibeik <milad.khanibeik@exxeta.com>
2020-04-25 15:56:45 +0200

    - a user with the given email address is created when the "Post Confirmation" event is triggered.

--- lambdas/post-user/src/index.ts
+++ lambdas/post-user/src/index.ts
@@ -1,88 +1,45 @@
 require('dotenv').config();
 
-import {AddressData} from "../../../common/help-on-spot-models/dist/models/RestModels";
-import {LambdaResponse, lambdaResponse} from "../../../common/help-on-spot-models/dist/utils/lambdaResponse";
-import {Address, Connection, Qualification, User, In, Repository} from "../../../common/help-on-spot-models/dist";
+import {User, Repository} from "../../../common/help-on-spot-models/dist";
 import {Database} from "../../../common/help-on-spot-models/dist/utils/Database";
 
+export const handler = async (event: any, context: any, callback: any) => {
+  console.log(event);
 
-export interface LambdaInputEvent {
-  body: string
-}
-
-interface UserData {
-  firstName: string;
-  lastName: string;
-  isGPSLocationAllowed: boolean;
-  email: string;
-  avatar: string;
-  qualifications: string[];
-  address: AddressData
-}
-
-export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> => {
-  console.log(JSON.parse(event.body))
-
-  const userData: UserData = JSON.parse(event.body)
+  const email = event.request.userAttributes.email;
+  if (!email) {
+    callback('no email was found in the user attributes');
+  }
 
   const db = new Database();
   const connection = await db.getConnection();
 
   if (!connection) {
-    return lambdaResponse(500, 'No Database connection');
+    callback('no database connection');
   }
 
-  const userRepository = connection!.getRepository(User);
+  const userRepository = connection!.getRepository(User); 
 
-  const email = await findEmail(userData.email, userRepository);
-
-  if (email) {
-    await db.disconnect(connection)
-    return lambdaResponse(400, 'User with email already exists!')
+  if (await findByEmail(email, userRepository)) {
+    console.log(`A user with email ${email} already exists!`);
+    await db.disconnect(connection);
+    callback('email is already taken');
   }
 
-  const qualifications = await findQualifications(userData.qualifications, connection);
-
-  if (qualifications.length !== userData.qualifications.length) {
-    await db.disconnect(connection)
-    return lambdaResponse(400, 'Some qualifications are not valid!');
-  }
-
-  const user = new User(
-    userData.firstName,
-    userData.lastName,
-    userData.isGPSLocationAllowed,
-    userData.email,
-    userData.avatar,
-    qualifications
-  );
-
-  if (userData.address) {
-    user.address = new Address(userData.address);
-  }
-
+  const user = new User(email, false, []);
 
   try {
-    const savedUser: User = await userRepository.save(user);
-    return lambdaResponse(200, JSON.stringify(savedUser));
+    await userRepository.save(user);
+    callback(null, event);
   } catch (e) {
-    console.log(`Error during lambda execution: ${JSON.stringify(e)}`)
-    return lambdaResponse(500, JSON.stringify(e));
-  }
-  finally {
-      await db.disconnect(connection)
+    console.log(`Error during lambda execution: ${JSON.stringify(e)}`);
+    callback('user cannot be saved');
+  } finally {
+    await db.disconnect(connection);
   }
 }
 
-async function findQualifications(qualifications: string[], connection: Connection): Promise<Qualification[]> {
-  const qualificationRepository = connection.getRepository(Qualification);
-
-  return qualificationRepository.find({
-    key: In(qualifications)
-  });
-}
-
-async function findEmail(email: string, userRepository: Repository<User>): Promise<User | undefined> {
+async function findByEmail(email: string, userRepository: Repository<User>): Promise<User | undefined> {
   return userRepository.findOne({
     where: { email: email }
   });
--- lambdas/post-user/src/test.ts
+++ lambdas/post-user/src/test.ts
@@ -1,27 +1,33 @@
-import {  handler, LambdaInputEvent } from './index'
+import {  handler } from './index'
 
 (async function () {
-  const user = {
-    firstName: "Max",
-    lastName: "Mustermann",
-    isGPSLocationAllowed: true,
-    email: "test@tedst.de",
-    avatar: "picture.jpg",
-    address: {
-      houseNumber: "12 a",
-      city: "Munich",
-      street: "Hauptstraße",
-      postalCode: "089",
-      country: "Germany"
+  const event = {
+    version: "1",
+    region: "eu-central-1",
+    userPoolId: "eu-central-1_Ydd5t1uwk",
+    userName: "max.mustermann",
+    callerContext: {
+      awsSdkVersion: "aws-sdk-unknown-unknown",
+      clientId: "1u97cevfn0bm393ngb9tffqbd5"
     },
-   qualifications: [
-      "driversLicence",
-      "medicalEducation"
-    ]
-  }
-  const requestObject: LambdaInputEvent = {
-    body: JSON.stringify(user)
-  }
-  const result = await handler(requestObject)
-  console.log(JSON.stringify(result))
+    triggerSource: "PostAuthentication_Authentication",
+    request: {
+      userAttributes: {
+        sub: "b3673ee2-9mju-49cc-t67u-44d05e5f5861",
+        email_verified: true,
+        email: "max.musterman@example.com"
+      },
+      newDeviceUsed: false
+    },
+    response: {}
+  };
+
+  const context = {
+    functionVersion: "1",
+    functionName: "post-confirmation-lambda"
+  };
+
+  await handler(event, context, (error: any, response: any) => { 
+    console.log(response);
+  });
 })()
