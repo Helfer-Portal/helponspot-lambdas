298ade0eba9e7c3a6bb39acbba07c4d9750c8ddc
Duc Tung Tong <tungtongduc@gmail.com>
2020-04-28 18:52:51 +0200

    configure get-users-id-request

--- lambdas/get-users-id-request/src/test.ts
+++ lambdas/get-users-id-request/src/test.ts
@@ -1,82 +1,85 @@
-import Organisation from '../../../common/help-on-spot-models/dist/entity/Organisation'
+import Organisation from '/opt/nodejs/common/help-on-spot-models/dist/entity/Organisation'
 
 require('dotenv').config()
 
 import { handler, LambdaInputEvent } from './index'
-import { Database } from '../../../common/help-on-spot-models/dist/utils/Database'
-import { Address, Qualification, User } from '../../../common/help-on-spot-models/dist'
-import { AddressData, OrganisationData, RequestData } from '../../../common/help-on-spot-models/dist/models/RestModels'
-import Request from '../../../common/help-on-spot-models/dist/entity/Request'
-;(async function () {
-    const connection = await new Database().getConnection()
-    const userRepo = connection!.getRepository(User)
-    const orgRepo = connection!.getRepository(Organisation)
-    const adata: AddressData = {
-        country: 'germany',
-        city: 'myyCity',
-        postalCode: '123',
-        houseNumber: '1',
-        street: 'street'
-    }
-    const address = await connection.getRepository(Address).save(new Address(adata))
+import { Database } from '/opt/nodejs/common/help-on-spot-models/dist/utils/Database'
+import { Address, Qualification, User } from '/opt/nodejs/common/help-on-spot-models/dist'
+import { AddressData, OrganisationData, RequestData } from '/opt/nodejs/common/help-on-spot-models/dist/models/RestModels'
+import Request from '/opt/nodejs/common/help-on-spot-models/dist/entity/Request'
 
-    const qualifications = await connection.getRepository(Qualification).find()
-    const randomEmail = Math.random().toString(36).substring(7) + '@test'
-    const user = new User(
-        randomEmail,
-        false,
-        qualifications.filter((q) => q.key === 'physicallyFit'),
-        'Test',
-        'User',
-        '',
-        1
-    )
-    user.address = address
-    await userRepo.save(user)
-    const addressData: AddressData = { city: 'c', country: 'c', houseNumber: 'h', postalCode: '1', street: 's' }
-    const organ: OrganisationData = { address: addressData, email: '@lo', logoPath: 'l', name: 'o', responsibles: [] }
-    const organisation = await orgRepo.save(new Organisation(organ, [user]))
-
-    const requestData: RequestData = {
-        title: 'huhu',
-        address: {
-            street: 'string',
-            postalCode: 'string',
-            houseNumber: 'string',
+describe("get qualifications handler", () => {
+    it("should return status 200", async () => {
+        const connection = await new Database().getConnection()
+        const userRepo = connection!.getRepository(User)
+        const orgRepo = connection!.getRepository(Organisation)
+        const adata: AddressData = {
+            country: 'germany',
             city: 'myyCity',
-            country: 'string'
-        },
-        description: 'desc',
-        endDate: '2004-07-11',
-        startDate: '2004-07-12',
-        isActive: false,
-        qualificationKeys: ['physicallyFit']
-    }
-    await connection.getRepository(Request).save(new Request(requestData, organisation, []))
-    await connection.getRepository(Request).save(
-        new Request(
-            requestData,
-            organisation,
-            qualifications.filter((q) => q.key === 'physicallyFit')
-        )
-    )
-    await connection.getRepository(Request).save(
-        new Request(
-            requestData,
-            organisation,
-            qualifications.filter((q) => q.key === 'driversLicence')
-        )
-    )
-
-    await connection.close()
-
-    const requestObject: LambdaInputEvent = {
-        pathParameters: {
-            userId: user.id!
+            postalCode: '123',
+            houseNumber: '1',
+            street: 'street'
         }
-    }
+        const address = await connection.getRepository(Address).save(new Address(adata))
 
-    const result = await handler(requestObject)
+        const qualifications = await connection.getRepository(Qualification).find()
+        const randomEmail = Math.random().toString(36).substring(7) + '@test'
+        const user = new User(
+            randomEmail,
+            false,
+            qualifications.filter((q) => q.key === 'physicallyFit'),
+            'Test',
+            'User',
+            '',
+            1
+        )
+        user.address = address
+        await userRepo.save(user)
+        const addressData: AddressData = { city: 'c', country: 'c', houseNumber: 'h', postalCode: '1', street: 's' }
+        const organ: OrganisationData = { address: addressData, email: '@lo', logoPath: 'l', name: 'o', responsibles: [] }
+        const organisation = await orgRepo.save(new Organisation(organ, [user]))
 
-    console.log(JSON.stringify(result))
-})()
+        const requestData: RequestData = {
+            title: 'huhu',
+            address: {
+                street: 'string',
+                postalCode: 'string',
+                houseNumber: 'string',
+                city: 'myyCity',
+                country: 'string'
+            },
+            description: 'desc',
+            endDate: '2004-07-11',
+            startDate: '2004-07-12',
+            isActive: false,
+            qualificationKeys: ['physicallyFit']
+        }
+        await connection.getRepository(Request).save(new Request(requestData, organisation, []))
+        await connection.getRepository(Request).save(
+            new Request(
+                requestData,
+                organisation,
+                qualifications.filter((q) => q.key === 'physicallyFit')
+            )
+        )
+        await connection.getRepository(Request).save(
+            new Request(
+                requestData,
+                organisation,
+                qualifications.filter((q) => q.key === 'driversLicence')
+            )
+        )
+
+        await connection.close()
+
+        const requestObject: LambdaInputEvent = {
+            pathParameters: {
+                userId: user.id!
+            }
+        }
+
+        const result = await handler(requestObject)
+        expect(result.statusCode).toEqual(200)
+    })
+})
+
