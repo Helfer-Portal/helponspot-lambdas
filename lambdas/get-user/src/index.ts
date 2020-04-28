dd010fe4f09733a95b2141edd9354245e6c7b0e3
Andreas Voit <AndreasVoit@gmx.de>
2020-04-28 19:18:08 +0200

    #61: GET user by email

--- lambdas/get-user/src/index.ts
+++ lambdas/get-user/src/index.ts
@@ -1,22 +1,20 @@
-require('dotenv').config()
-import { User } from '../../../common/help-on-spot-models/dist'
-import { Database } from '../../../common/help-on-spot-models/dist/utils/Database'
-import { LambdaResponse, lambdaResponse } from '../../../common/help-on-spot-models/dist/utils/lambdaResponse'
+
+import {Connection, User} from '../../../common/help-on-spot-models/dist'
+import {Database} from '../../../common/help-on-spot-models/dist/utils/Database'
+import {LambdaResponse, lambdaResponse} from '../../../common/help-on-spot-models/dist/utils/lambdaResponse'
 
 export interface LambdaInputEvent {
     pathParameters: any
 }
+
+const relevantRelations: string[] = ['address', 'qualifications', 'organisations']
 
 export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> => {
     const userId = event.pathParameters.userId
     const db = new Database()
     const connection = await db.getConnection()
     try {
-        const repo = connection.getRepository(User)
-        const user: User | undefined = await repo.findOne({
-            where: { id: userId },
-            relations: ['address', 'qualifications', 'organisations']
-        })
+        const user = await findUser(userId, connection)
         if (!user) {
             return lambdaResponse(404, 'User not found!')
         }
@@ -27,4 +25,24 @@ export const handler = async (event: LambdaInputEvent): Promise<LambdaResponse> 
     } finally {
         await db.disconnect(connection)
     }
+}
+
+async function findUser(userId: string, connection: Connection): Promise<User | undefined> {
+    const repo = connection.getRepository(User)
+    if (isEmail(userId)) {
+        return await repo.findOne({
+            where: {email: userId},
+            relations: relevantRelations
+        })
+    } else {
+        return await repo.findOne({
+            where: {id: userId},
+            relations: relevantRelations
+        })
+    }
+
+}
+
+function isEmail(userId: string): boolean {
+    return userId.includes('@')
 }
