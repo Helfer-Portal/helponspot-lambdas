d5bdd478db380188a44fd4bc555f2efe0ba70160
Andreas Voit <AndreasVoit@gmx.de>
2020-05-17 10:31:52 +0200

    #83: cleanup

--- lambdas/patch-user/src/test.ts
+++ lambdas/patch-user/src/test.ts
@@ -1,31 +1,26 @@
+require('dotenv').config()
 import { handler, LambdaInputEvent } from './index'
 import { Database } from '/opt/nodejs/common/help-on-spot-models/dist/utils/Database'
 import { User } from '/opt/nodejs/common/help-on-spot-models/dist'
 
-describe('delete organisation handler', () => {
-    it('should return status 200', async () => {
-        const connection = await new Database().getConnection()
-        const userRepo = connection.getRepository(User)
 
-        const randomEmail = Math.random().toString(36).substring(7) + '@test'
-        const user = await userRepo.save(new User(randomEmail, false, [], 'Test', 'User', '', 1))
-        await connection.close()
+async function setup(): Promise<User> {
+    const connection = await new Database().getConnection()
+    const userRepo = connection.getRepository(User)
 
+    const randomEmail = Math.random().toString(36).substring(7) + '@test'
+    const user = await userRepo.save(new User(randomEmail, false, [], 'Test', 'User', '', 1))
+    await connection.close()
+    return user
+}
+
+describe('patch user handler',  () => {
+
+    it('Change name', async () => {
+        const user = await setup()
         const patchUser = {
-            firstName: 'Max neu',
-            lastName: 'Mustermann neu',
-            isGPSLocationAllowed: true,
-            email: 'test@neu123.de',
-            avatar: 'new_picture.jpg',
-            travellingDistance: 17,
-            address: {
-                houseNumber: '1',
-                city: 'München',
-                street: 'Heinrich-Lübke-Straße',
-                postalCode: '81737',
-                country: 'Deutschland'
-            },
-            qualifications: ['medicalEducation', 'driversLicence']
+            firstName: 'Max',
+            lastName: 'Mustermann',
         }
         const requestObject: LambdaInputEvent = {
             body: JSON.stringify(patchUser),
@@ -35,5 +30,30 @@ describe('delete organisation handler', () => {
         }
         const result = await handler(requestObject)
         expect(result.statusCode).toEqual(200)
+        expect(result.body).toContain('Max')
+
     })
+
+    // it('Change address', async () => {
+    //     const user = await setup()
+    //     const patchUser = {
+    //         address: {
+    //             houseNumber: '1',
+    //             city: 'München',
+    //             street: 'Heinrich-Lübke-Straße',
+    //             postalCode: '81737',
+    //             country: 'Deutschland'
+    //         }
+    //     }
+    //     const requestObject: LambdaInputEvent = {
+    //         body: JSON.stringify(patchUser),
+    //         pathParameters: {
+    //             userId: user.id
+    //         }
+    //     }
+    //     const result = await handler(requestObject)
+    //     expect(result.statusCode).toEqual(200)
+    //     expect(result.body).toContain('Heinrich-Lübke-Straße')
+    //
+    // })
 })
